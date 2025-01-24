c5ff43530a033d15971cda0d9026a0f89ea32ad2
mariafaria0511 <maria.faria0511@gmail.com>
2025-01-24 17:22:20 -0300

    Add files via upload
    Variable Fun Assignment

---
+++ main (1).py
@@ -0,0 +1,44 @@
+#math in python
+from math import remainder
+value1= int(input("Please enter a number\n"))
+value2 = int(input("Please enter another number\n"))
+multiplied= value1 * value2
+print("The multiplication result is {}".format(multiplied))
+
+value1= int(input("Please enter a number\n"))
+value2 = int(input("Please enter another number\n"))
+divided= value1 / value2
+print("The dividion result is {}".format(divided))
+
+value1= int(input("Please enter a number\n"))
+value2 = int(input("Please enter another number\n"))
+divided= value1 // value2
+print("The dividion result is {}".format(divided))
+
+value1= int(input("Please enter a number\n"))
+value2 = int(input("Please enter another number\n"))
+module= value1 % value2
+print("The module result is {}".format(module))
+
+value1= int(input("Please enter a number\n"))
+value2 = int(input("Please enter another number\n"))
+sum = value1 + value2
+print("The sum is {}".format(sum))
+
+value1= int(input("Please enter a number\n"))
+value2 = int(input("Please enter another number\n"))
+subtract = value1 - value2
+print("The subtraction result is {}".format(subtract))
+
+
+
+
+
+
+
+
+
+
+
+
+
